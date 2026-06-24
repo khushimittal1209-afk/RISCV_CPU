b6b78ea1d9e81eddc2fb25a84eaa5a7597c02ec6
kem-kem-kem <km.909azw@gmail.com>
2026-06-24 23:39:04 +0530

    Working single-cycle RISC-V CPU

--- rtl/instruction_memory.v
+++ rtl/instruction_memory.v
@@ -1,18 +1,13 @@
 module instruction_memory(
-    input [31:0] address,
+    input  [31:0] address,
     output [31:0] instruction
 );
 
 reg [31:0] memory [0:255];
+
 initial begin
-
-memory[0] = 32'h12345097;// auipc x1,0x12345
-
-
-
+    $readmemh("program.mem", memory);
 end
-
-
 
 assign instruction = memory[address[9:2]];
 
